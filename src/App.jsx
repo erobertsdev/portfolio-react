c253be226658d8c22b13e27968710f5aa5364fed
Elias Roberts <elias@eroberts.dev>
2023-03-05 04:20:52 -0700

    styled header component

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
       <Header />
+      <Navbar />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import './styles/Header.css'
+
 function Header() {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <h1 className="header-title">&lt;<span id="header-e">E</span><span id="header-slash">&#47;</span><span id="header-r">R</span>&gt;</h1>
     </div>
   );
 }
